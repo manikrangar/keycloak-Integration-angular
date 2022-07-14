719c7726c6d6deb8b2d69ad5050e5574888b029e
manikrangar <manikrangarx@gmail.com>
2022-07-14 10:28:07 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+// import { AuthGuard } from './utility/app.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"",
+    component:WelcomeComponent,
+  },
+  {
+    path:"dashboard",
+    component:DashboardComponent,
+    // canActivate:[AuthGuard],
+  },
+  {
+    path:".about",
+    component:AboutComponent,
+    // canActivate:[AuthGuard],
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
